79203386e751725129a26f32afd4e4edc143d543
Filippo831 <filippoarduini83@gmail.com>
2025-02-25 16:04:56 +0100

    added TLS support, still need to be fixed

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Filippo831/reverse_proxy
 
 go 1.24.0
+
+require (
+	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+)
--- reverse_proxy.go
+++ reverse_proxy.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"net/url"
+	"strings"
+	"time"
+
+	"golang.org/x/net/http2"
 )
 
 var PORT int = 8081
@@ -13,37 +19,71 @@ var PORT int = 8081
 func main() {
 	fmt.Sprintln("starting reverse proxy at port %d", PORT)
 
-    demoURL, err := url.Parse("http://127.17.0.1:8080")
-    if err != nil {
-        log.Fatal(err)
-    }
+	demoURL, err := url.Parse("https://127.17.0.1:8096")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    proxy := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        fmt.Println("incoming request")
-        r.Host = demoURL.Host
-        r.URL.Host = demoURL.Host
-        r.URL.Scheme = demoURL.Scheme
-        r.RequestURI = ""
+	proxy := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("incoming request")
+		r.Host = demoURL.Host
+		r.URL.Host = demoURL.Host
+		r.URL.Scheme = demoURL.Scheme
+		r.RequestURI = ""
 
-        resp, err := http.DefaultClient.Do(r)
+		client_host, _, _ := net.SplitHostPort(r.RemoteAddr)
+		r.Header.Set("X-Forwarded-For", client_host)
 
-        if err != nil {
-            w.WriteHeader(http.StatusInternalServerError)
-            fmt.Fprint(w, err)
-            return
-        }
+		tr := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
+		http2.ConfigureTransport(tr)
 
-        for key, values := range resp.Header {
-            for _, value := range values {
-                w.Header().Add(key, value)
-            }
-        }
+		resp, err := http.DefaultClient.Do(r)
 
-        io.Copy(w, resp.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, err)
+			return
+		}
 
-    })
+		for key, values := range resp.Header {
+			for _, value := range values {
+				w.Header().Add(key, value)
+			}
+		}
 
-	if err := http.ListenAndServe(":8081", proxy); err != nil {
+		done := make(chan bool)
+		go func() {
+			for {
+				select {
+				case <-time.Tick(10 * time.Millisecond):
+					w.(http.Flusher).Flush()
+				case <-done:
+					return
+				}
+			}
+		}()
+
+		trailerKeys := []string{}
+
+		for key := range resp.Trailer {
+			trailerKeys = append(trailerKeys, key)
+		}
+
+		w.Header().Set("Trailer", strings.Join(trailerKeys, ","))
+
+		for key, values := range resp.Trailer {
+			for _, value := range values {
+				w.Header().Set(key, value)
+			}
+		}
+
+		w.WriteHeader(resp.StatusCode)
+		io.Copy(w, resp.Body)
+
+		close(done)
+	})
+
+	if err := http.ListenAndServeTLS(":8081", "reverse_proxy.rsa.crt", "reverse_proxy.rsa.key", proxy); err != nil {
 		log.Fatal(err)
 	}
 }
